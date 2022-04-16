d5317e8b1730c74211a2f4bde4ebee23c40b952f
maxiCaballo <caballomaxi@gmail.com>
2022-04-16 00:30:37 -0300

    limpieza

--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,22 +1,17 @@
 import styles from "./styles.css";
 import { useState } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
 
 function ScrollToTop() {
   const [scrollPx, setScrollPx] = useState(0);
-  AOS.init();
 
   window.addEventListener("scroll", () => {
     setScrollPx(window.scrollY);
   });
-  console.log(scrollPx);
 
   return (
     <div
       className={`scrollToTopContainer ${scrollPx < 500 && "d-none"}`}
       onClick={() => window.scrollTo(0, 0)}
-      data-aos="fade-left"
     >
       <button className="cta">
         <span className="hover-underline-animation"> Scroll to top </span>
